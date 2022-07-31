cff852d43822f6fbd6c6044f21d291d632cd9a14
Sergey Evseev <mresv@ya.ru>
2022-07-31 17:26:36 +0300

    Exp 006 added

---
+++ Exp006_Giri/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 3;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Максимальное значение = ");
+Console.Write(max);
