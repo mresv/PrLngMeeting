a2ff3fde49976744abd7b5716d69e63b447bc68a
Sergey Evseev <mresv@ya.ru>
2022-07-31 17:15:04 +0300

    Exp005 added

---
+++ Exp005_If_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Имя пользователя");
+string user = Console.ReadLine();
+if (user.ToLower() == "маша")
+{
+Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.Write(user);
+}
