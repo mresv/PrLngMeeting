42678e5cfbf39de478d9b1c081382131322b0d4c
Sergey Evseev <mresv@ya.ru>
2022-07-31 16:38:44 +0300

    folder Exp002 renamed

--- Exp003_Sum/Program.cs
+++ Exp003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 3;
-int numB = 5;
+﻿int numA = new Random().Next(1,10); //1 2 3 .. 9
+Console.WriteLine(numA);
+int numB = new Random().Next(1,10);
+Console.WriteLine(numB);
 int summ = numA + numB;
 Console.WriteLine(summ);
--- Exp004_Div/Program.cs
+++ Exp004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numA = 3;
+double numB = 5;
+double div = numA / numB;
+Console.WriteLine(div);
